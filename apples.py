2cb5a410ff054b30781aa89ab401e85eeffc3fb2
anushkasaxena27 <saxenaanushka27@gmail.com>
2022-08-01 19:31:31 +0530

    delete

--- apples.py
+++
@@ -1,7 +0,0 @@
-print ('you see an apples seller')
-q = input('do you like apples?')
-
-if q == 'yes':
-    print('purchase 2 kg apples')
-if q == 'no':
-    print ('you do not purchase apples')
--- variables.py
+++
@@ -1,9 +0,0 @@
-name = 'Anushka'
-college = 'BBDITM'
-age = 2000
-pi = 3.14
-
-print (name)
-print (college)
-print(age)
-print (pi)
