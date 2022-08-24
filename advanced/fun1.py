e5534b8a2e8f705e2a08bf905446c9a8ddb454b4
anushkasaxena27 <saxenaanushka27@gmail.com>
2022-08-24 14:26:31 +0530

    function

--- advanced/fun1.py
+++ advanced/fun1.py
@@ -22,7 +22,23 @@ greetings('Namaste Dosto')
 def calc_area(w,h):
     area = w*h
     print('area:  ', area)
-
+# passing values to function
 calc_area(10,20)
 calc_area(3,5)
 calc_area(100,200)
+
+
+def calc_area_v2(w,h):
+    area = w*h
+    return area
+#display
+print(calc_area_v2(10,20))
+print(calc_area_v2(3,5))
+
+#storing return value in a variable
+ans = calc_area_v2(10,20)
+print (ans)
+
+#using return values in a expression
+ans = calc_area_v2(3,5)+ calc_area_v2(10,20)
+print(ans)
