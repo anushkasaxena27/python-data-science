bcbcfc38cd06b1a26df0c942c30fc3bc74d7f9f0
anushkasaxena27 <saxenaanushka27@gmail.com>
2022-09-06 21:31:34 +0530

    set

--- data_structures/remove_set.py
+++ data_structures/remove_set.py
@@ -4,7 +4,7 @@ print (my_set)
 my_set.discard(4)   #discard an item
 print(my_set)
 
-my_set.remove(6)  # remove an element
+my_set.remove(6)  # remove an element throws error if element not present
 print(my_set)
 
 my_set.pop() #pops a random item
