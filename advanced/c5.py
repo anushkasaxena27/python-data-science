dc8398f2fd126614c443d07d2aa397e2509a15c1
anushkasaxena27 <saxenaanushka27@gmail.com>
2022-09-06 21:26:27 +0530

    classes and objects

---
+++ advanced/c5.py
@@ -0,0 +1,28 @@
+from tkinter.simpledialog import SimpleDialog
+
+
+class Shape:
+    def __init__(self,side) -> None:
+        self.side = side 
+
+    def info (self):
+        return f'shape with {self.side}'
+
+class Rectangle (Shape):
+
+    def __init__(self,l,w): 
+        super().__init__(l)
+        self.w = w
+
+    def area(self):
+        return self.side *self.w  #side is taken as length
+
+    def info(self):
+        return f'rectangle with l = {self.side} & w = {self.w}'
+    
+ob1 = Shape(4)
+print(ob1.info())
+
+ob2 = Rectangle(4,5)
+print(ob2.area())
+print(ob2.info())
